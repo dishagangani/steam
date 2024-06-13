09ba7d16780e12f9dc320e43d8714b5c1ca5a419
dishagangani <ganganidisha7@gmail.com>
2024-06-13 05:54:16 +0530

    first

---
+++ js/script.js
@@ -0,0 +1,54 @@
+ $(document).ready(function(){
+ setTimeout(function(){
+    $('.loader').fadeOut();
+},2000);
+});
+
+$(window).scroll(function(){
+    var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();
+  
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+
+    $(document).ready(function(){
+        $('.owl-carousel').owlCarousel({
+            items:2,
+            loop:true,
+            margin:20,
+            nav:true,
+            navText:['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
+            dots:false,
+        });
+
+        AOS.init();
+
+        });
+
+        // to_top
+
+        var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+$(document).ready(function(){
+  setTimeout(function(){
+      $('.pre_loader').fadeOut();
+  },1000);
+    })
+        
+  });
+
+  
